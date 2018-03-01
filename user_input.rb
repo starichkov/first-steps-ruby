bf21555fc769dffb9098b15ba2db6e2ac098ba8b
Vadim Starichkov <vadim_starichkov@epam.com>
2018-03-01 15:03:31 +0300

    Added user input examples

---
+++ user_input.rb
@@ -0,0 +1,10 @@
+# convert input to numbers
+num = gets.to_i
+puts "Input: #{num}"
+
+float = gets.to_f
+puts "Input float: #{float}"
+
+# input ends with pressing Enter
+name = gets.chomp
+puts "Hello, #{name}!"
