43a952d4b87e2414ac80532b79f07beede969e25
Vadim Starichkov <vadim_starichkov@epam.com>
2018-03-02 13:28:28 +0300

    Added lambdas example

--- procs.rb
+++ procs.rb
@@ -1,3 +1,5 @@
+# proc examples
+
 pow = Proc.new do |x, p|
   puts x ** p
 end
@@ -14,3 +16,22 @@ end
 
 arr = [2, 4, 8]
 do_with_each_in_array(arr, pow2)
+
+# lambda example
+
+hello = lambda {|name| puts "Hello, #{name}!"}
+hello.call "Vadim Starichkov"
+
+# difference between procs and lambdas
+
+proc2 = Proc.new do |x, y|
+  puts "#{x} x #{y}"
+end
+
+lambda2 = lambda {|x, y| puts "#{x} x #{y}"}
+
+# does not require all arguments, set to nil if does not set
+proc2.call 10
+
+# require all arguments, raise error if does not set
+lambda2.call 10
